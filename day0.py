3db53fd7973978d44e2856053c40e15a623854ff
liyc1989 <lyc19890807@126.com>
2019-05-22 21:02:38 +0800

    day0.py
    Signed-off-by: liyc1989 <lyc19890807@126.com>

---
+++ day0.py
@@ -0,0 +1,12 @@
+"""
+第一个Python程序 -hello world!
+Version：0.1
+Author：李玉晨
+Date： 2019-5-22
+"""
+
+print('hello world!','hello python')
+print('hello world!','hello python',sep='')
+print('hello world!','hello python',sep='!!')
+
+print('hello world!','hello python', sep = ',', end = '')
